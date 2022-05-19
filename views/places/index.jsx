366184ef526571ce9f3c3c799bdf268308c43b23
unknown <hk32077@gmail.com>
2022-05-19 15:45:48 -0500

    Adding Bootstrap grid to index page

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,26 @@ const Def = require('../default')
 function index(data) {
     let formattedPlaces = data.places.map(place => {
         return (
-            <div>
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
+                <p clasName="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name} />
+                <p clasName="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
     return (
         <Def>
             <main>
-                <h1>Index Page</h1>
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
                 {formattedPlaces}
+                </div>
+                
             </main>
         </Def>
     )
